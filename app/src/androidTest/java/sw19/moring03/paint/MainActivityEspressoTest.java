7476d04604b9621ae5ec257dbefc734e37d88667
Magdalena Mayerhofer <m.mayerhofer@student.tugraz.at>
2019-03-20 10:29:22 +0100

    PAINT-001 test to check visibility of tool chooser button [MM, RT]

---
+++ app/src/androidTest/java/sw19/moring03/paint/MainActivityEspressoTest.java
@@ -0,0 +1,26 @@
+package sw19.moring03.paint;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityEspressoTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testToolChooserButtonVisibility() {
+        onView(withId(R.id.toolChooserButton)).check(matches(isDisplayed()));
+    }
+
+}
