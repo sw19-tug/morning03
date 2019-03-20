846022ede17af39ce30091c53d05aa7085b09e08
Michael Gombocz <michael.gombocz@student.tugraz.at>
2019-03-20 10:48:42 +0100

    PAINT-002a implemented base tool class [GM,EV]

---
+++ app/src/main/java/sw19/moring03/paint/tools/Tools.java
@@ -0,0 +1,12 @@
+package sw19.moring03.paint.tools;
+
+import android.graphics.PointF;
+
+import java.util.List;
+
+public abstract class Tools {
+    ///adds drawable tool to list
+    public boolean Draw(List<PointF> points){
+        return true;
+    }
+}
