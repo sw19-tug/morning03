ebbed92b2fbb038e77b742320da9a23363538739
Thomas Reichel <treichel@student.tugraz.at>
2019-03-23 10:28:40 +0100

    PAINT-001 added menu with icon-button for choosing a drawing tool [MM, RT]

--- app/src/main/java/sw19/moring03/paint/MainActivity.java
+++ app/src/main/java/sw19/moring03/paint/MainActivity.java
@@ -2,6 +2,9 @@ package sw19.moring03.paint;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.toolChooserButton) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
