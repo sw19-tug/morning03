9bc5a730169c81f63cfeed726aa65dc34b626c09
edel3 <valentinedelsbrunnerx@gmail.com>
2019-03-23 12:34:59 +0100

    PAINT-002 point tool, basic implementation [EV,GM]

---
+++ app/src/main/java/sw19/moring03/paint/Views/DrawingView.java
@@ -0,0 +1,78 @@
+package sw19.moring03.paint.Views;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import sw19.moring03.paint.tools.PointTool;
+import sw19.moring03.paint.tools.Tools;
+
+public class DrawingView extends View {
+    private List<Tools> objectsToPaint;
+    private Paint movePaint;
+
+    public DrawingView(Context c, AttributeSet attributeSet) {
+        super(c, attributeSet);
+
+        movePaint = new Paint();
+        movePaint.setAntiAlias(true);
+        movePaint.setColor(Color.BLACK);
+        movePaint.setStyle(Paint.Style.STROKE);
+        movePaint.setStrokeWidth(4.0f);
+        objectsToPaint = new ArrayList<>();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float xCoord = event.getX();
+        float yCoord = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_UP:
+                invalidate();
+                break;
+            case MotionEvent.ACTION_DOWN:
+                selectTool();
+                addPoint(xCoord, yCoord);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                addPoint(xCoord, yCoord);
+                invalidate();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if(objectsToPaint != null) {
+            for(Tools tool : objectsToPaint) {
+
+                if(tool instanceof PointTool) {
+                    tool.draw(canvas, movePaint);
+                }
+                /// TODO: insert other tools(line, path)
+            }
+        }
+    }
+
+    private void addPoint(float x, float y) {
+        PointF point = new PointF(x, y);
+        objectsToPaint.get(objectsToPaint.size()-1).addPoint(point);
+    }
+
+    private void selectTool() {
+        objectsToPaint.add(new PointTool());
+    }
+}
---
+++ app/src/main/java/sw19/moring03/paint/tools/PointTool.java
@@ -0,0 +1,22 @@
+package sw19.moring03.paint.tools;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import java.util.ArrayList;
+
+public class PointTool extends Tools {
+    public PointTool() {points = new ArrayList<>();};
+
+    @Override
+    public boolean draw(Canvas canvas, Paint paint) {
+        if(points != null && points.size() < 1)
+            return false;
+        
+        float xCoord = points.get(0).x;
+        float yCoord = points.get(0).y;
+
+        canvas.drawPoint(xCoord, yCoord, paint);
+        return true;
+    }
+}
