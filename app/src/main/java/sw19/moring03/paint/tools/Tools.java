23f80f36856e530693fb9a8e94aae8d60d7dcb35
Maximilian Theiner <maximilian.theiner@student.tugraz.at>
2019-03-23 11:18:18 +0100

    PAINT-003 Updated base class [PL,MT]

--- app/src/main/java/sw19/moring03/paint/tools/Tools.java
+++ app/src/main/java/sw19/moring03/paint/tools/Tools.java
@@ -1,11 +1,21 @@
 package sw19.moring03.paint.tools;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.PointF;
 
 import java.util.List;
 
 public abstract class Tools {
-    public boolean draw(List<PointF> points){
-        return true;
+    protected List<PointF> points;
+
+    public abstract boolean draw(Canvas canvas, Paint paint);
+
+    public void addPoint(PointF point) {
+        points.add(point);
+    }
+
+    public List<PointF> getPoints() {
+        return points;
     }
 }
