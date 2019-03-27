77cf5abadc25aeeb52e5d8a8f1c4e51a56eed08f
Magdalena Mayerhofer <m.mayerhofer@student.tugraz.at>
2019-03-27 09:35:25 +0100

    PAINT-001 add test to check if menu closes after a tool is chosen [MM, RT]

--- app/src/androidTest/java/sw19/moring03/paint/MainActivityEspressoTest.java
+++ app/src/androidTest/java/sw19/moring03/paint/MainActivityEspressoTest.java
@@ -1,5 +1,6 @@
 package sw19.moring03.paint;
 
+import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -16,6 +17,7 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.fail;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityEspressoTest {
@@ -51,6 +53,11 @@ public class MainActivityEspressoTest {
         onView(withId(R.id.drawPointButton)).perform(click());
 
         assertEquals(activityTestRule.getActivity().getChosenTool(), Tool.DRAW_POINT);
+        try {
+            onView(withId(R.id.toolChooserMenu)).check(matches(isDisplayed()));
+            fail("Expected exception not thrown");
+        } catch (NoMatchingViewException ignored) {
+        }
     }
 
     @Test
@@ -59,6 +66,11 @@ public class MainActivityEspressoTest {
         onView(withId(R.id.drawLineButton)).perform(click());
 
         assertEquals(activityTestRule.getActivity().getChosenTool(), Tool.DRAW_LINE);
+        try {
+            onView(withId(R.id.toolChooserMenu)).check(matches(isDisplayed()));
+            fail("Expected exception not thrown");
+        } catch (NoMatchingViewException ignored) {
+        }
     }
 
     @Test
@@ -67,5 +79,62 @@ public class MainActivityEspressoTest {
         onView(withId(R.id.drawPathButton)).perform(click());
 
         assertEquals(activityTestRule.getActivity().getChosenTool(), Tool.DRAW_PATH);
+        try {
+            onView(withId(R.id.toolChooserMenu)).check(matches(isDisplayed()));
+            fail("Expected exception not thrown");
+        } catch (NoMatchingViewException ignored) {
+        }
+    }
+
+    @Test
+    public void testCircleToolSelected() {
+        onView(withId(R.id.toolChooserButton)).perform(click());
+        onView(withId(R.id.drawCircleButton)).perform(click());
+
+        assertEquals(activityTestRule.getActivity().getChosenTool(), Tool.DRAW_CIRCLE);
+        try {
+            onView(withId(R.id.toolChooserMenu)).check(matches(isDisplayed()));
+            fail("Expected exception not thrown");
+        } catch (NoMatchingViewException ignored) {
+        }
+    }
+
+    @Test
+    public void testFillToolSelected() {
+        onView(withId(R.id.toolChooserButton)).perform(click());
+        onView(withId(R.id.drawFillButton)).perform(click());
+
+        assertEquals(activityTestRule.getActivity().getChosenTool(), Tool.FILL);
+        try {
+            onView(withId(R.id.toolChooserMenu)).check(matches(isDisplayed()));
+            fail("Expected exception not thrown");
+        } catch (NoMatchingViewException ignored) {
+        }
+    }
+
+    @Test
+    public void testEraseToolSelected() {
+        onView(withId(R.id.toolChooserButton)).perform(click());
+        onView(withId(R.id.eraserButton)).perform(click());
+
+        assertEquals(activityTestRule.getActivity().getChosenTool(), Tool.ERASER);
+        try {
+            onView(withId(R.id.toolChooserMenu)).check(matches(isDisplayed()));
+            fail("Expected exception not thrown");
+        } catch (NoMatchingViewException ignored) {
+        }
+    }
+
+    @Test
+    public void testRectangleToolSelected() {
+        onView(withId(R.id.toolChooserButton)).perform(click());
+        onView(withId(R.id.drawRectangleButton)).perform(click());
+
+        assertEquals(activityTestRule.getActivity().getChosenTool(), Tool.DRAW_RECTANGLE);
+        try {
+            onView(withId(R.id.toolChooserMenu)).check(matches(isDisplayed()));
+            fail("Expected exception not thrown");
+        } catch (NoMatchingViewException ignored) {
+        }
     }
 }
