9bd190e025a53cad5b94f101b43f312d972b8fa0
patlov <patrick.lovric@student.tugraz.at>
2019-03-27 08:54:58 +0100

    PAINT-003 test change + gradle adjustments [MT,PL]

--- app/src/test/java/sw19/moring03/paint/LineTest.java
+++ app/src/test/java/sw19/moring03/paint/LineTest.java
@@ -1,4 +1,5 @@
 package sw19.moring03.paint;
+
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PointF;
@@ -21,111 +22,65 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LineTest {
-        private Canvas canvas;
-        private Paint paint;
+    private Canvas canvas;
+    private Paint paint;
 
-        @Before
-        public void startUp() throws Exception {
-            canvas = new Canvas();
-            paint = new Paint();
-        }
+    @Before
+    public void startUp() throws Exception {
+        canvas = new Canvas();
+        paint = new Paint();
+    }
 
-        @Test
-        public void testLineSimple()
-        {
-            LineTool tool = new LineTool();
+    @Test
+    public void testLineSimple() {
+        LineTool tool = new LineTool();
 
-            tool.addPoint(new PointF(15, 15));
-            tool.addPoint(new PointF(30, 30));
-            tool.addPoint(new PointF(60, 60));
-            tool.addPoint(new PointF(90, 90));
+        tool.addPoint(new PointF(15, 15));
+        tool.addPoint(new PointF(30, 30));
+        tool.addPoint(new PointF(60, 60));
+        tool.addPoint(new PointF(90, 90));
 
-            Assert.assertTrue(tool.draw(canvas, paint));
-        }
+        assertTrue(tool.draw(canvas, paint));
+    }
 
-        @Test
-        public void testDrawLine()
-        {
-            int expectedLines = 1;
+    @Test
+    public void testDrawLine() {
+        int expectedLines = 1;
 
-            LineTool tools = new LineTool();
+        LineTool tools = new LineTool();
 
-            tools.addPoint(new PointF(10, 10));
-            tools.addPoint(new PointF(20, 20));
-            tools.addPoint(new PointF(30, 30));
-            tools.addPoint(new PointF(40,40));
+        tools.addPoint(new PointF(10, 10));
+        tools.addPoint(new PointF(20, 20));
+        tools.addPoint(new PointF(30, 30));
+        tools.addPoint(new PointF(40, 40));
 
-            canvas = Mockito.mock(Canvas.class);
+        canvas = Mockito.mock(Canvas.class);
 
-            tools.draw(canvas, paint);
+        tools.draw(canvas, paint);
 
-            Mockito.verify(canvas, Mockito.times(expectedLines)).drawLine(Mockito.anyFloat(),
-                    Mockito.anyFloat(), Mockito.anyFloat(), Mockito.anyFloat(), Mockito.any(Paint.class));
-        }
+        Mockito.verify(canvas, Mockito.times(expectedLines)).drawLine(Mockito.anyFloat(),
+                Mockito.anyFloat(), Mockito.anyFloat(), Mockito.anyFloat(), Mockito.any(Paint.class));
+    }
 
-        @Test
-            public void testAddPoints()
-        {
-                List<PointF> expectedPoints = new ArrayList<>();
+    @Test
+    public void testAddPoints() {
+        List<PointF> expectedPoints = new ArrayList<>();
 
-                expectedPoints.add(new PointF(10, 10));
-                expectedPoints.add(new PointF(20, 20));
-                expectedPoints.add(new PointF(30, 30));
-                expectedPoints.add(new PointF(30, 30));
+        expectedPoints.add(new PointF(10, 10));
+        expectedPoints.add(new PointF(20, 20));
+        expectedPoints.add(new PointF(30, 30));
+        expectedPoints.add(new PointF(30, 30));
 
-                LineTool tool = new LineTool();
+        LineTool tool = new LineTool();
 
-                tool.addPoint(expectedPoints.get(0));
-                tool.addPoint(expectedPoints.get(1));
-                tool.addPoint(expectedPoints.get(2));
-                tool.addPoint(expectedPoints.get(3));
+        tool.addPoint(expectedPoints.get(0));
+        tool.addPoint(expectedPoints.get(1));
+        tool.addPoint(expectedPoints.get(2));
+        tool.addPoint(expectedPoints.get(3));
 
-                List<PointF> points = tool.getPoints();
-                Assert.assertArrayEquals(expectedPoints.toArray(), points.toArray());
+        List<PointF> points = tool.getPoints();
+        assertArrayEquals(expectedPoints.toArray(), points.toArray());
 
-        }
+    }
 
-        @Test
-        public void testRemoveLines()
-        {
-            List<PointF> lineList = new ArrayList<>();
-
-            lineList.add(new PointF(30, 30));
-            lineList.add(new PointF(40, 40));
-            lineList.add(new PointF(50, 50));
-            lineList.add(new PointF(60, 60));
-
-
-
-            lineList.remove(new PointF(30, 30));
-            lineList.remove(new PointF(40, 40));
-
-            List<PointF> lineList2 = new ArrayList<>();
-
-            lineList2.add(new PointF(50, 50));
-            lineList2.add(new PointF(60, 60));
-
-            Assert.assertArrayEquals(lineList.toArray(), lineList2.toArray());
-
-        }
-
-        @Test
-            public void testMultipleLines()
-        {
-                List<PointF> lineList = new ArrayList<>();
-
-                lineList.add(new PointF(30, 30));
-                lineList.add(new PointF(40, 40));
-                lineList.add(new PointF(50, 50));
-                lineList.add(new PointF(60, 60));
-
-                List<PointF> lineList2 = new ArrayList<>();
-
-            lineList2.add(new PointF(30, 30));
-            lineList2.add(new PointF(40, 40));
-            lineList2.add(new PointF(50, 50));
-            lineList2.add(new PointF(60, 60));
-
-                Assert.assertArrayEquals(lineList.toArray(), lineList2.toArray());
-        }
 }
