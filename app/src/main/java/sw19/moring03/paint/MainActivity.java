95df976605ffbf0ac2b4a6f79da288d6a918c0d3
Magdalena Mayerhofer <m.mayerhofer@student.tugraz.at>
2019-03-27 10:07:13 +0100

    PAINT-001 removed autogenerated comments and unused imports [MM, RT]

--- app/src/main/java/sw19/moring03/paint/MainActivity.java
+++ app/src/main/java/sw19/moring03/paint/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 
 import sw19.moring03.paint.utils.Tool;
 
@@ -26,19 +25,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.toolChooserButton) {
             toolChooserMenu = ToolChooserMenuBottomSheetDialog.newInstance();
             toolChooserMenu.show(getSupportFragmentManager(), "toolChooserMenu");
