4e2fcad5b580e94072f5abac9fece5ba6d7c9440
Michael Gombocz <michael.gombocz@student.tugraz.at>
2019-03-27 12:46:23 +0100

    PAINT-005 added base class for tool [GM,TM]

---
+++ app/src/main/java/sw19/moring03/paint/tools/EraseTool.java
@@ -0,0 +1,29 @@
+package sw19.moring03.paint.tools;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import java.util.ArrayList;
+
+public class EraseTool extends PathTool {
+
+    public EraseTool() {
+        points = new ArrayList<>();
+    }
+
+    @Override
+    public boolean draw(Canvas canvas, Paint paint) {
+
+        if(points == null || points.size() < 2) {
+            return false;
+        }
+
+        Paint erasePaint = paint;
+        erasePaint.setColor(backgroundColor);
+        for(int i = 1; i < points.size(); i++) {
+            canvas.drawLine(points.get(i-1).x, points.get(i-1).y, points.get(i).x, points.get(i).y, erasePaint);
+        }
+
+        return true;
+    }
+}
--- app/src/main/java/sw19/moring03/paint/tools/Tools.java
+++ app/src/main/java/sw19/moring03/paint/tools/Tools.java
@@ -1,6 +1,7 @@
 package sw19.moring03.paint.tools;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
 
@@ -9,6 +10,8 @@ import java.util.List;
 public abstract class Tools {
 
     protected List<PointF> points;
+
+    static protected int backgroundColor = Color.WHITE;
 
     public abstract boolean draw(Canvas canvas, Paint paint);
 
