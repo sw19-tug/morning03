c0c5bc7b21f3d2d72c4ccd53f050f29d4966daec
Michael Gombocz <michael.gombocz@barcodepony.net>
2019-05-27 09:44:43 +0200

    PAINT-022 added test for SprayCanTool.draw() [GM,MM]

---
+++ app/src/test/java/sw19/moring03/paint/SprayCanTest.java
@@ -0,0 +1,34 @@
+package sw19.moring03.paint;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PointF;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import sw19.moring03.paint.tools.PointTool;
+
+import static junit.framework.TestCase.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SprayCanTest {
+    private Canvas canvas;
+    private Paint paint;
+
+    @Before
+    public void setUp() {
+        canvas = new Canvas();
+        paint = new Paint();
+    }
+
+    @Test
+    public void basicPointDrawTest() {
+        SprayCanTool tool = new SprayCanTool();
+        tool.addPoint(new PointF(5, 5));
+        assertTrue(tool.draw(canvas, paint));
+    }
+}
