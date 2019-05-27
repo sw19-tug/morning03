fd14880f175967fa75489384a3fb5f3875fd213f
edel3 <valentinedelsbrunnerx@gmail.com>
2019-05-27 17:36:33 +0200

    PAINT-0021 removed unnecessary initialization  [EV, SP]

--- app/src/main/java/sw19/moring03/paint/tools/TextTool.java
+++ app/src/main/java/sw19/moring03/paint/tools/TextTool.java
@@ -2,7 +2,6 @@ package sw19.moring03.paint.tools;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
 import android.graphics.Typeface;
@@ -16,8 +15,6 @@ public class TextTool extends Tools {
 
     public TextTool() {
         points = new ArrayList<>();
-        strokeWidth = 5;
-        color = Color.BLACK;
     }
 
     public TextTool(int col, int strkW, Context context) {
