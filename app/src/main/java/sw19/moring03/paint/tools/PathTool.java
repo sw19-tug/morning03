2fe7edb8aae82ed2d00ad0912c2fb93d9bd92086
Michelle Perkonigg <michelle.perkonigg@student.tugraz.at>
2019-05-29 11:29:12 +0200

    PAINT-027 better the path tool [PM,SP]

--- app/src/main/java/sw19/moring03/paint/tools/PathTool.java
+++ app/src/main/java/sw19/moring03/paint/tools/PathTool.java
@@ -3,21 +3,38 @@ package sw19.moring03.paint.tools;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 
 import java.util.ArrayList;
 
+import sw19.moring03.paint.utils.PointF;
+
 public class PathTool extends Tools {
+    private Path path;
 
     public PathTool() {
         points = new ArrayList<>();
         strokeWidth = 5;
         color = Color.BLACK;
+        path = new Path();
     }
 
     public PathTool(int col, int strkW) {
         points = new ArrayList<>();
         strokeWidth = strkW;
         color = col;
+        path = new Path();
+    }
+
+    @Override
+    public void addPoint(PointF point) {
+        if (path.isEmpty()) {
+            path.moveTo(point.x, point.y);
+        }
+        else {
+            path.lineTo(point.x, point.y);
+        }
+        super.addPoint(point);
     }
 
     @Override
@@ -27,9 +44,7 @@ public class PathTool extends Tools {
             return false;
         }
 
-        for (int i = 1; i < points.size(); i++) {
-            canvas.drawLine(points.get(i - 1).x, points.get(i - 1).y, points.get(i).x, points.get(i).y, paint);
-        }
+        canvas.drawPath(path, paint);
 
         return true;
     }
