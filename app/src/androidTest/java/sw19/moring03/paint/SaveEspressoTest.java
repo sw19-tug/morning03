a87f706cae8e99c22d0ba092502264c764511eea
Michael Gombocz <michael.gombocz@barcodepony.net>
2019-06-05 08:55:38 +0200

    PAINT-028 replaced icon with vector graphic asset [EV,GM]

--- app/src/androidTest/java/sw19/moring03/paint/SaveEspressoTest.java
+++ app/src/androidTest/java/sw19/moring03/paint/SaveEspressoTest.java
@@ -3,6 +3,8 @@ package sw19.moring03.paint;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -13,6 +15,7 @@ import org.junit.runner.RunWith;
 import java.io.IOException;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -27,11 +30,25 @@ public class SaveEspressoTest {
 
     @Test
     public void testSaveButtonExists() {
-        onView(withId(R.id.saveButton)).check(matches(isDisplayed()));
+        //onView(withId(R.id.saveButton)).check(matches(isDisplayed()));
+        try {
+            onView(withId(R.id.saveButton)).check(matches(isDisplayed()));
+        } catch (NoMatchingViewException e) {
+            openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+            onView(withId(R.string.saveButton)).check(matches(isDisplayed()));
+        }
     }
 
     @Test
     public void testImageSaved() throws IOException {
+        try {
+            onView(withId(R.id.saveButton)).perform(click());
+        } catch (NoMatchingViewException e) {
+            openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+            onView(withId(R.id.saveButton)).perform(click());
+        }
+
+
         onView(withId(R.id.saveButton)).perform(click());
 
         Uri imageURI = Uri.parse(activityTestRule.getActivity().getLastSavedImageURI());
